26bc0fbaab816e0bc0b24ed7c24aef291eb97889
Bartek <jobs.gaudynek@gmail.com>
2020-07-26 22:19:16 +0200

    utworzony projekt

---
+++ src/main/java/sharpenApplication/SharpenApplication.java
@@ -0,0 +1,13 @@
+package sharpenApplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SharpenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SharpenApplication.class, args);
+	}
+
+}
