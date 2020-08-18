0e0eb8948c241819e500fde3fcea57eccafed793
Bartek <jobs.gaudynek@gmail.com>
2020-08-18 14:56:05 +0200

    koszyk polaczony z backendem

--- src/main/java/sharpenControllers/shopController.java
+++ src/main/java/sharpenControllers/shopController.java
@@ -3,15 +3,32 @@ package sharpenControllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import sharpenProducts.Product;
 import sharpenProducts.storeManager;
 
 @Controller
 public class shopController {
 	
+	//dodac warunek w przypadku pustego koszyka
+	
 	@GetMapping("/basket")
 	public String pageBasket(Model model) {
 		model.addAttribute("basket", storeManager.getBasket().getBasket());
 		model.addAttribute("cena", storeManager.getBasket().getSuma());
+		return "basket";
+	}
+	
+	@PostMapping("/basket")
+	public String pageBasketUpdate(@RequestParam(value = "delete") String index, Model model) {
+		model.addAttribute("basket", storeManager.getBasket().getBasket());
+		storeManager.basket.getBasket().remove(Integer.parseInt(index));
+		model.addAttribute("cena", storeManager.getBasket().getSuma());
+		System.out.println(storeManager.basket.getBasket().size());
+		System.out.println(storeManager.basket.getSuma());
+		
 		return "basket";
 	}
 	
